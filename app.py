86ac37e718baaf9cfd899d8e1848a1dc919de9c3
RedShadow30 <148815559+RedShadow30@users.noreply.github.com>
2025-05-07 15:12:16 -0500

    First commit: Implemented KindBites Template

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from multiapp import MultiApp
+from apps import market, articles, chatbot
+
+
+app = MultiApp()
+
+css_style = """
+<style>
+  .stApp {
+    background: linear-gradient(to right, #00B822, #00ADE6); /* Green to Blue gradient */
+  }
+</style>
+"""
+
+st.markdown(css_style, unsafe_allow_html=True)
+st.markdown("""
+# Welcome to KindBites 🍜🍕🍫
+
+Pitch here!
+
+""")
+
+
+app.add_app("Market", market.app)
+app.add_app("Articles", articles.app)
+app.add_app("Chatbot", chatbot.app)
+
+app.run()
