f9bf91270dcbd8f9c5b57b73731407caf76c095d
RedShadow30 <148815559+RedShadow30@users.noreply.github.com>
2025-05-09 16:33:30 -0500

    RAG Chatbot completed. UI can be refined.

--- apps/chatbot.py
+++ apps/chatbot.py
@@ -1,4 +1,41 @@
 import streamlit as st
+import rag.rag as rag
+from streamlit_chat import message
+from streamlit_extras.colored_header import colored_header
+from streamlit_extras.add_vertical_space import add_vertical_space
+
+st.set_page_config(page_title="Chatbot", page_icon="🤖")
+
 
 def app():
     st.title("Chatbot Page")
+
+    with st.chat_message("assistant"):
+        st.write("Hey there! What questions do you have about food sustainability?")
+    
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # Assign user input to prompt var
+    if prompt := st.chat_input("Type your question here..."):
+        # Display user msg in chat
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        
+        # Get response from the chatbot
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = rag.get_answer(prompt)
+                st.markdown(response)
+
+        # Add assistant message to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
---
+++ rag/rag.py
@@ -0,0 +1,69 @@
+import re
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from pymongo import MongoClient
+from langchain_community.vectorstores import MongoDBAtlasVectorSearch
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import ollama
+from langchain.chains import RetrievalQA
+
+#langchain-mongodb
+#langchain-ollama
+
+# Create client and collection for mongo connection
+client = MongoClient("mongodb+srv://finding:nemo@algoarena-cluster.mzcfclp.mongodb.net/")
+db = client["vector-store"]
+collection = client["vector-store"]["embeddings"]
+
+# Create embeddings with FastEmbed
+embeddings = FastEmbedEmbeddings()
+
+# Load iff collection is empty
+if collection.count_documents({}) == 0:
+    # Load PDF file 
+    loader = PyPDFLoader("Food_Sus.pdf")
+    pages = loader.load()
+
+    # Split docs into chunks
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(pages)
+
+    # Clean up text: Remove punc and #s
+    for text in texts:
+        text.page_content = re.sub(r'[^\w %]|_', '', text.page_content, flags=re.UNICODE)
+        print(text.page_content)
+
+    # Store docs in mongo
+    docsearch = MongoDBAtlasVectorSearch.from_documents(texts, embeddings, collection=collection)
+
+else:
+    # Load existing data
+    docsearch = MongoDBAtlasVectorSearch(embedding=embeddings, collection=collection)
+
+# Custom prompt template for LLM to know how to approach a question
+custom_prompt_template = '''Consider the following article:
+
+{context}
+
+Now, based on the article, answer the following question:
+
+{question}
+
+Provide a concise and accurate response below. If you cannot answer the question based on the article, say "I'm not sure I can answer this. I'd happy to answer question about promoting food sustainability though!".
+
+'''
+
+prompt = PromptTemplate(template=custom_prompt_template, input_variables=['context', 'question'])
+
+llm = ollama.Ollama(model="llama2", temperature=0.6)
+qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=docsearch.as_retriever(search_type="similarity", search_kwargs={"k": 1}),
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": prompt}
+)
+
+def get_answer(question):
+    return qa.invoke(question)["result"]
